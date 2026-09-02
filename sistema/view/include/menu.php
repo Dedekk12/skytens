386553b4ebc1dc6b77e5dc499b51c79e167f4887
MuriloIscuissati <muriloi.ifpr2024@gmail.com>
2026-09-02 16:37:03 -0300

    Update menu.php

--- sistema/view/include/menu.php
+++ sistema/view/include/menu.php
@@ -4,17 +4,14 @@ require_once(__DIR__ . "/../../util/Config.php");
 
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Skytems</a>
+        <a class="navbar-brand" href="<?= BASE_URL ?>index.php">Skytems</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">  
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
+                    <a class="nav-link" href="<?= BASE_URL ?>view/personagem/inserir.php">Criação de Personagem</a>
                 </li>
             </ul>
             
@@ -22,13 +19,14 @@ require_once(__DIR__ . "/../../util/Config.php");
             <ul class="navbar-nav">
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Dropdown
+                        Listas
                     </a>
                     <ul class="dropdown-menu dropdown-menu-end">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><a class="dropdown-item" href="<?= BASE_URL ?>view/personagem/listar.php">Personagens</a></li>
                         <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        <li><a class="dropdown-item" href="#">Poderes</a></li>
+                        <li><a class="dropdown-item" href="#">Conjuntos</a></li>
+                        <li><a class="dropdown-item" href="#">Raças</a></li>
                     </ul>
                 </li>
             </ul>
